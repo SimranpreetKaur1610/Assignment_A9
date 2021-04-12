c617beea59928a0105fd50cff7bde068bce45d7f
Simranpreetkaur <harsim610@gmail.com>
2021-04-12 01:11:53 -0400

    initializing and declaring the variables

--- src/Assignment_9/CalculateGrossPay.java
+++ src/Assignment_9/CalculateGrossPay.java
@@ -1,9 +1,21 @@
 package Assignment_9;
 
+import java.util.Scanner;
+
 public class CalculateGrossPay
 {
     public static void main(String[] args)
     {
+        Scanner read = new Scanner(System.in);
+        System.out.print("Calculate the Monthly Sales \n\n");
+
+        System.out.print("Enter the monthly sales : "); //To get the Monthly sales
+        double sales = read.nextDouble();
+
+        System.out.print("Enter the amount of advanced pay : "); //To get Advance pay.
+        double advance = read.nextDouble();
+
+        double commissionRate=0.0;
 
     }
 }
